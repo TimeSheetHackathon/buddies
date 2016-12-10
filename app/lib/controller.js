1d9764c81e24ba79e289ecab6b687be15aa8d74f
Lalit <lalitpan@thoughtworks.com>
2016-12-10 12:31:14 +0530

    Added userInfo

--- app/lib/controller.js
+++ app/lib/controller.js
@@ -1,14 +1,25 @@
-var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var users = {}
 
 app.post('/fill', function(req, res) {
-	
+	var data = req.body()
 });
+
+
+var addUserInfo = function(req, res) {
+	var info = req.body;
+	if(!users.hasOwnProperty(info.name))
+		users[info.name] = {name: info.name, pwd: info.psw};
+	res.send({added : true});
+}
+
 
 app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({ extended: true}));
+
+app.post('/timeCardEntry/creds', addUserInfo);
 
 module.exports = function(req, res) {
 	app(req, res);
